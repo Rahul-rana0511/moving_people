f1bb4447effa23b95be36774bbf4764137ebcd2d
rahul <rahul.rana@codobux.in>
2026-06-13 19:43:43 +0530

    updated code

--- moving_people.js
+++ moving_people.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT; // fallback port
 // ✅ CORS should be here before any routes
 app.use(cors({
   origin: '*',
-  methods: ['GET','POST','PUT','DELETE'],
+  methods: ['GET','POST','PUT','DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
